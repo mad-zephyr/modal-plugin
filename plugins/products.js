f0c6b98efba6731ae3153601a93000295a18371c
Mad.Zephyr <v21400@gmail.com>
2021-08-10 01:26:13 +0300

    progress

--- plugins/products.js
+++ plugins/products.js
@@ -10,6 +10,21 @@ $.products = function (options) {
 
 	const listener = event => {
 		if (event.target.dataset.btn === 'price'){
+
+			const card = event.target.parentNode.parentNode
+
+			console.log(card.querySelector('.card-title'))
+ 
+			const img = card.querySelector('img').getAttribute('src')
+			const title = card.querySelector('.card-title') 
+
+			// console.log(title)
+			// modal.setContent(`
+			// 	<div>${console.log(this.products)}</div>
+			// `)
+			
+			modal.open()
+
 			console.log('Price')
 			
 		} else if (event.target.dataset.btn === 'delete') {
@@ -41,16 +56,16 @@ function _createProduct(options) {
 			$card.classList.add('card')
 			$card.style.width = "18rem"
 			$card.insertAdjacentHTML('beforeend', `
-				<img  style=" object-fit: cover;" src="${card.img}" class="card-img-top" alt="${card.title}">
+				<img  style=" object-fit: cover;" src="${card.img}" class="card-img-top" alt="${card.title}" data-img="${card.img}">
 				<div class="card-body">
-					<h5 class="card-title" style="display: flex; justify-content: space-between">${card.title} <span>${card.price} Mdl</span></h5>
-					<a href="#" class="btn btn-primary" data-btn="price" >Посмотреть Цену</a>
+					<h5 class="card-title" style="display: flex; justify-content: space-between" data-title="${card.title}">${card.title} <span cla>${card.price} Mdl</span></h5>
+					<a href="#" class="btn btn-primary" data-btn="price" >Подробнее</a>
 					<a href="#" class="btn btn-danger" data-btn="delete" >Удалить</a>
 				</div>
 			`) 
 			products.push($card)
 	})
 
-	console.log( products)
+	console.log(products)
 	return products
 }
